f26d23e860ac419159705163bd4478c29a530e74
RitualSquash501 EstebanLR10 <estebilr91@gmail.com>
2021-11-25 19:30:04 -0600

    inicialización de programación

--- Calculadora/Calculadora/Form1.Designer.cs
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnsumar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsumar
+            // 
+            this.btnsumar.AccessibleName = "";
+            this.btnsumar.Location = new System.Drawing.Point(322, 153);
+            this.btnsumar.Name = "btnsumar";
+            this.btnsumar.Size = new System.Drawing.Size(182, 94);
+            this.btnsumar.TabIndex = 1;
+            this.btnsumar.Text = "+";
+            this.btnsumar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsumar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnsumar;
     }
 }
 
