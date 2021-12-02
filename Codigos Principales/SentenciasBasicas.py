0a662e7f8b79dbecd025de362cfd4ca83891e1df
RitualSquash501 EstebanLR10 <estebilr91@gmail.com>
2021-12-01 20:05:28 -0600

    Primera clase de pitón

---
+++ Codigos Principales/SentenciasBasicas.py
@@ -0,0 +1,34 @@
+print("Hola mundo!")
+print("Soy Esteban!")
+num1=1
+letra="Hola mundo"
+num2=2
+print(type(num1))
+print(type(num2))
+print(type(letra))
+num1=int(input("Digite un numero: "))
+num2=int(input("Digite un numero: "))
+print(num1,num2)
+#Averiguar cual es el mayor de dos digitos
+if (num1>num2):
+  print(f'este es el numero mayor{num1}')
+else: 
+  print(f'este es el numero mayor {num2}')
+#  cout<<num1 =print(num1)
+# cin>>num1
+
+#como funciona el ciclo while
+#contador
+cont=1
+
+while cont!=11:
+  print(cont)
+  cont+=1
+  
+else:
+  print("Descontamos")
+  while cont!=1:
+    print(cont)
+    cont-=1
+  else:
+      print("Se acabo esta mierda")
---
+++ Profesor/Primera_Clase_SentenciaBasica.py
@@ -0,0 +1,25 @@
+print("Hola mundo!")
+print("Soy Esteban!")
+
+num1=1
+letra="Hola mundo"
+num2=2
+
+print(type(num1))
+print(type(num2))
+print(type(letra))
+num1,num2=float(input("Digite un numero"))
+#num2=float(input("Digite un numero"))
+
+#  cout<<num1 =print(num1)
+# cin>>num1
+
+#Averiguar cual es el mayor de dos digitos
+#Averiguar cual es el mayor de dos digitos
+# c++=if sentencia {
+
+# }
+if num1>num2:
+  print(f'Este es el numero mayor{num1}')
+else:
+  print(f'Este es el numero mayor{num2}')
